9d96cee075a92dfaf41ddf8b784f5ce946a4c9e4
Srikara Thikkireddy <130203062+srik4442@users.noreply.github.com>
2023-10-15 00:09:56 +0530

    Add files via upload

---
+++ src/components/Contact.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+const Contact = () => {
+  return (
+    <section>
+      <h2>Contact</h2>
+      <p>
+        I'm always open to new opportunities and collaborations. 
+      </p>
+      <ul>
+        <li>
+          <strong>Email:</strong> <a href="mailto:jeevanscience273@gmail.comjeevanscience273@gmail.com</a>
+        </li>
+        <li>
+          <strong>LinkedIn:</strong> <a href="linkedin.com/in/jeevanba273/">click here</a>
+        </li>
+        <li>
+          <strong>GitHub:</strong> <a href="https://github.com/jeevanba273" target="_blank" rel="noopener noreferrer">click here</a>
+        </li>
+       
+      </ul>
+      <p>
+        I'm looking forward to connecting with you!
+      </p>
+    </section>
+  );
+};
+
+
+export default Contact;
