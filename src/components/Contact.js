a079201a198ef35ded933e10526d2bb7b7f0f947
Srikara Thikkireddy <130203062+srik4442@users.noreply.github.com>
2023-11-09 23:00:43 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,13 +10,13 @@ const Contact = () => {
       </p>
       <ul>
         <li>
-          <strong>Email:</strong> <a href="mailto:nikki405024@gmail.com">nikki405024@gmail.com</a>
+          <strong>Email:</strong> <a href="mailto:vasanth.21bce8588@vitapstudent.ac.in">vasanth.21bce8588@vitapstudent.ac.in</a>
         </li>
         <li>
-          <strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/nikhil-reddy-yennapusala-6356aa239?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">click here</a>
+          <strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/vaddadi-vasanth-70419a21a/?originalSubdomain=in">click here</a>
         </li>
         <li>
-          <strong>GitHub:</strong> <a href="https://github.com/Nikhil405024" target="_blank" rel="noopener noreferrer">click here</a>
+          <strong>GitHub:</strong> <a href="https://github.com/vasanthvvsc" target="_blank" rel="noopener noreferrer">click here</a>
         </li>
        
       </ul>
